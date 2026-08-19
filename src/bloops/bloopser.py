276a01d66eef47566adedce03b310ad10d8c9dc7
Bubu-Droid <fufudadw@gmail.com>
2026-08-20 02:11:20 +0530

    feat: organize asy conversion and move validation functions to another file

--- src/bloops/bloopser.py
+++ src/bloops/bloopser.py
@@ -1,16 +1,9 @@
-import json
-import pathlib
 import re
-import subprocess
-from typing import cast
 
 from bloops.bracer import (
     get_error_line,
     gobble_inside_delim,
 )
-
-# FIX: gotta move the error raises to validator function
-# and also check for duplicate asy filenames
 
 
 def convert_to_html(
@@ -18,12 +11,9 @@ def convert_to_html(
     open_delim_start_index: int,
     open_delim: re.Pattern[str],
     close_delim: re.Pattern[str],
-    in_dir: pathlib.Path | None = None,
-    out_dir: pathlib.Path | None = None,
 ) -> str:
     tag_and_args = get_tag_and_args(text, open_delim_start_index, open_delim)
-    tag = tag_and_args[0]
-    args = tag_and_args[1]
+    tag, args = tag_and_args
 
     content: list[str] = []
     optional_args_list: list[str] = []
@@ -33,79 +23,7 @@ def convert_to_html(
 
     if tag in ["asy", "code", "color", "img", "list", "quote", "url"]:
         if tag == "asy":
-            # FIX: move this entire code block to validator function
             asy_dict = args
-            if "src" not in asy_dict:
-                error_line = get_error_line(text, open_delim_start_index)
-                raise ValueError(
-                    'Missing mandatory parameter "src."'
-                    + "\n\n"
-                    + f"{error_line[0]}: {error_line[1]}"
-                )
-            if "label" not in asy_dict:
-                error_line = get_error_line(text, open_delim_start_index)
-                raise ValueError(
-                    'Missing mandatory parameter "label."'
-                    + "\n\n"
-                    + f"{error_line[0]}: {error_line[1]}"
-                )
-            if not (in_dir and out_dir):
-                raise TypeError(
-                    "Input and output directory paths are mandatory arguments."
-                )
-            build_dir = in_dir / "build/"
-            build_dir.mkdir(exist_ok=True)
-            asy_cache_file = build_dir / "cache.json"
-            if not asy_cache_file.exists():
-                asy_cache_file.touch()
-                with asy_cache_file.open("w", encoding="utf-8") as f:
-                    _ = f.write("{}")
-            with asy_cache_file.open("r", encoding="utf-8") as f:
-                asy_cache_content = cast(dict[str, str], json.load(f))
-            # if asy_dict["filename"] in asy_cache_content:
-            #     error_line = get_error_line(text, open_delim_start_index)
-            #     raise ValueError(
-            #         'Missing mandatory parameter "A diagram with the same filename already exists."'
-            #         + "\n\n"
-            #         + f"{error_line[0]}: {error_line[1]}"
-            #     )
-            changed = False
-            if asy_dict["label"] not in asy_cache_content:
-                asy_cache_content[asy_dict["label"]] = inner_content
-                changed = True
-            else:
-                if asy_cache_content[asy_dict["label"]] != inner_content:
-                    asy_cache_content[asy_dict["label"]] = inner_content
-                    changed = True
-            asy_code_file = build_dir / f"{asy_dict['label']}.asy"
-
-            if changed:
-                with asy_code_file.open("w", encoding="utf-8") as f:
-                    _ = f.write(inner_content)
-
-                try:
-                    _ = subprocess.run(
-                        [
-                            "asy",
-                            asy_code_file.absolute(),
-                            "-f",
-                            "svg",
-                            "-o",
-                            out_dir.absolute(),
-                        ],
-                        capture_output=True,
-                        text=True,
-                        check=True,
-                    )
-                except subprocess.CalledProcessError as e:
-                    error_line = get_error_line(text, open_delim_start_index)
-                    error_message: str = cast(str, e.stderr)
-                    raise ValueError(
-                        f"{error_line[0]}: {error_line[1]}"
-                        + "\n\n"
-                        + error_message
-                    )
-
             content.append("<figure>")
             if "alt" in asy_dict:
                 optional_args_list.append(f'alt="{asy_dict["alt"]}"')
@@ -118,8 +36,10 @@ def convert_to_html(
                 if optional_args_list
                 else f'<img src="{asy_dict["src"]}">'
             )
-            if inner_content:
-                content.append(f"<figcaption>{inner_content}</figcaption>")
+            if "caption" in asy_dict:
+                content.append(
+                    f"<figcaption>{asy_dict['caption']}</figcaption>"
+                )
             content.append("</figure>")
 
         elif tag == "code":
@@ -309,211 +229,3 @@ def get_tag_and_args(
     }
 
     return (match.group(1), args_dict)
-
-
-def validate_args(
-    text: str,
-    open_delim_start_index: int,
-    open_delim: re.Pattern[str],
-    valid_args: list[str],
-    in_dir: pathlib.Path | None = None,
-    out_dir: pathlib.Path | None = None,
-) -> bool:
-    tag_and_args = get_tag_and_args(text, open_delim_start_index, open_delim)
-    tag = tag_and_args[0]
-    args = tag_and_args[1]
-
-    # learning c++ has made me so pedantic that i
-    # even wanna optimize this O(mn) search, son ;-;
-
-    for arg in args:
-        if arg not in valid_args:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                "Invalid argument provided."
-                + f"{error_line[0]}: {error_line[1]}"
-                + "\n\n"
-                + f"{', '.join(valid_args)} are the only valid arguments."
-            )
-
-    if tag == "asy":
-        asy_dict = args
-        if "src" not in asy_dict:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Missing mandatory parameter "src."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        if "label" not in asy_dict:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Missing mandatory parameter "label."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        if not (in_dir and out_dir):
-            raise TypeError(
-                "Input and output directory paths are mandatory arguments."
-            )
-        build_dir = in_dir / "build/"
-        build_dir.mkdir(exist_ok=True)
-        asy_cache_file = build_dir / "cache.json"
-        if not asy_cache_file.exists():
-            asy_cache_file.touch()
-            with asy_cache_file.open("w", encoding="utf-8") as f:
-                _ = f.write("{}")
-        # with asy_cache_file.open("r", encoding="utf-8") as f:
-        #     asy_cache_content = cast(dict[str, str], json.load(f))
-        # if asy_dict["filename"] in asy_cache_content:
-        #     error_line = get_error_line(text, open_delim_start_index)
-        #     raise ValueError(
-        #         'Missing mandatory parameter "A diagram with the same filename already exists."'
-        #         + "\n\n"
-        #         + f"{error_line[0]}: {error_line[1]}"
-        #     )
-
-    elif tag == "color":
-        color_dict = args
-        if "hex" not in color_dict:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Missing mandatory parameter "hex."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-
-    elif tag == "img":
-        img_dict = args
-        if "src" not in img_dict:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Missing mandatory parameter "src."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-
-    elif tag == "list":
-        list_dict = args
-        if "type" in list_dict and list_dict["type"] not in ["ul", "ol"]:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Invalid list type provided. Valid types are "ul" and "ol" only.'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        if "type" in list_dict and list_dict["type"] == "ol":
-            if "style" in list_dict and list_dict["style"] not in [
-                "1",
-                "A",
-                "a",
-                "I",
-                "i",
-            ]:
-                error_line = get_error_line(text, open_delim_start_index)
-                raise ValueError(
-                    'Invalid <ol> style provided. Valid ol styles are "1", "A", "a", "I", and "i" only.'
-                    + "\n\n"
-                    + f"{error_line[0]}: {error_line[1]}"
-                )
-        else:
-            if "style" in list_dict and list_dict["style"] not in [
-                "disc",
-                "circle",
-                "square",
-                "none",
-            ]:
-                error_line = get_error_line(text, open_delim_start_index)
-                raise ValueError(
-                    'Invalid <ul> style provided. Valid ul styles are "disc", "circle", "square", and "none" only.'
-                    + "\n\n"
-                    + f"{error_line[0]}: {error_line[1]}"
-                )
-
-    elif tag == "url":
-        url_dict = args
-        if "link" not in url_dict:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Missing mandatory parameter "link."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        if "target" in url_dict and url_dict["target"] not in [
-            "self",
-            "blank",
-        ]:
-            error_line = get_error_line(text, open_delim_start_index)
-            raise ValueError(
-                'Invalid target value provided. Target can only be "self" or "blank."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-
-    return args != {}
-
-
-def validate_and_setup_asy(
-    text: str,
-    open_delim: re.Pattern[str],
-    valid_args: list[str],
-    in_dir: pathlib.Path | None = None,
-    out_dir: pathlib.Path | None = None,
-):
-    index = 0
-    match = re.search(open_delim, text[index:])
-    label_list: list[str] = []
-
-    while match:
-        index = match.start()
-        args = get_tag_and_args(text, index, open_delim)[1]
-
-        for arg in args:
-            if arg not in valid_args:
-                error_line = get_error_line(text, index)
-                raise ValueError(
-                    "Invalid argument provided."
-                    + f"{error_line[0]}: {error_line[1]}"
-                    + "\n\n"
-                    + f"{', '.join(valid_args)} are the only valid arguments."
-                )
-
-        asy_dict = args
-        if "src" not in asy_dict:
-            error_line = get_error_line(text, index)
-            raise ValueError(
-                'Missing mandatory parameter "src."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        if "label" not in asy_dict:
-            error_line = get_error_line(text, index)
-            raise ValueError(
-                'Missing mandatory parameter "label."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-
-        if asy_dict["label"] in label_list:
-            error_line = get_error_line(text, index)
-            raise ValueError(
-                'Missing mandatory parameter "A diagram with the same filename already exists."'
-                + "\n\n"
-                + f"{error_line[0]}: {error_line[1]}"
-            )
-        # with asy_cache_file.open("r", encoding="utf-8") as f:
-        #     asy_cache_content = cast(dict[str, str], json.load(f))
-        # if asy_dict["filename"] in asy_cache_content:
-
-    if not (in_dir and out_dir):
-        raise TypeError(
-            "Input and output directory paths are mandatory arguments."
-        )
-
-    build_dir = in_dir / "build/"
-    build_dir.mkdir(exist_ok=True)
-    asy_cache_file = build_dir / "cache.json"
-    if not asy_cache_file.exists():
-        asy_cache_file.touch()
-        with asy_cache_file.open("w", encoding="utf-8") as f:
-            _ = f.write("{}")
