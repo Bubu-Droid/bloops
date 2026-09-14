c04b32128bb468535b8dfc8938f55f04c4d15644
Bubu-Droid <fufudadw@gmail.com>
2026-09-15 04:15:13 +0530

    tests: add return types and test for incorrect line no

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,12 +1,13 @@
+import pathlib
 import re
 
 import pytest
 
-from bloops.validator import validate_args
+from bloops.validator import validate_args, validate_tags_and_setup_asy
 
 
 class TestValidateArgs:
-    def test_invalid_args(self):
+    def test_invalid_args(self) -> None:
         text = r"""meow
 lorem [asy src=/home/bubu/meow.png label=hello meow=hi width=50 alt="meow neow" caption=this]some random stuff
 [/asy]"""
@@ -22,7 +23,7 @@ lorem [asy src=/home/bubu/meow.png label=hello meow=hi width=50 alt="meow neow" 
         assert "Invalid argument provided" in str(context.value)
         assert "are the only valid" in str(context.value)
 
-    def test_valid_args(self):
+    def test_valid_args(self) -> None:
         text = r"""meow
 lorem [asy src=/home/bubu/meow.png label=hello width=50 alt="meow neow" caption=this]some random stuff
 [/asy]"""
@@ -33,7 +34,7 @@ lorem [asy src=/home/bubu/meow.png label=hello width=50 alt="meow neow" caption=
             ["src", "label", "width", "alt", "caption"],
         )
 
-    def test_empty_args(self):
+    def test_empty_args(self) -> None:
         text = r"""meow
 lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
 [/asy]"""
@@ -45,7 +46,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
             ["src", "label", "width", "alt", "caption"],
         )
 
-    def test_asy_without_src(self):
+    def test_asy_without_src(self) -> None:
         text = r"""lorem [asy label=hello]some random stuff[/asy] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -57,7 +58,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert 'Missing mandatory parameter "src' in str(context.value)
 
-    def test_asy_without_label(self):
+    def test_asy_without_label(self) -> None:
         text = r"""lorem [asy src=/home/bubu/meow.png]some random stuff[/asy] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -69,7 +70,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert 'Missing mandatory parameter "label' in str(context.value)
 
-    def test_img_without_src(self):
+    def test_img_without_src(self) -> None:
         text = r"""lorem [img]some random stuff[/img] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -81,7 +82,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert "Missing mandatory parameter" in str(context.value)
 
-    def test_list_ul_with_invalid_style(self):
+    def test_list_ul_with_invalid_style(self) -> None:
         text = (
             r"""lorem [list type=ul style=meow]some random stuff[/list] lorem"""
         )
@@ -95,7 +96,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert "Invalid <ul> style provided" in str(context.value)
 
-    def test_list_ol_with_invalid_style(self):
+    def test_list_ol_with_invalid_style(self) -> None:
         text = (
             r"""lorem [list type=ol style=meow]some random stuff[/list] lorem"""
         )
@@ -109,7 +110,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert "Invalid <ol> style provided" in str(context.value)
 
-    def test_color_without_hex(self):
+    def test_color_without_hex(self) -> None:
         text = r"""lorem [color]some random stuff[/color] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -121,7 +122,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert "Missing mandatory parameter" in str(context.value)
 
-    def test_url_without_link(self):
+    def test_url_without_link(self) -> None:
         text = r"""lorem [url]some random stuff[/url] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -133,7 +134,7 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
         assert context.type is ValueError
         assert "Missing mandatory parameter" in str(context.value)
 
-    def test_url_with_invalid_target(self):
+    def test_url_with_invalid_target(self) -> None:
         text = r"""lorem [url link=https://www.bubudroid.me target=meow]some random stuff[/url] lorem"""
         with pytest.raises(ValueError) as context:
             _ = validate_args(
@@ -144,3 +145,56 @@ lorem [asy src=/home/bubu/meow.png label=hello]some random stuff
             )
         assert context.type is ValueError
         assert "Invalid target value provided" in str(context.value)
+
+    def test_incorrect_code(self) -> None:
+        text = r"""asdf [asy src="static/meow.svg" label=meow]
+        pair M = (0,0);
+        pair B = (-1/sqrt(3),0);
+        pair C = (1/sqrt(3),0);
+        pair A = dir(105);
+        pair P = (2B+C)/3;
+        pair Q = (B+2C)/3;
+        pair R = (2C+A)/3;
+        pair S = (C+2A)/3;
+        pair U = (2A+B)/3;
+        pair T = (A+2B)/3;
+        import graph;
+        import geometry;
+        size(11cm);
+        pen dps = linewidth(0.5) + fontsize(13);
+        defaultpen(dps);
+        draw(circle(M, 1), linewidth(0.5) + red);
+        draw(C--A, linewidth(0.5));
+        draw(A--B, linewidth(0.5));
+        draw(B--C, linewidth(0.5));
+        draw(circumcircle(R,S,Q), linewidth(0.5) + dashed + blue);
+        dot("$M$", M, NW);
+        dot("$B$", B, NW);
+        dot("$C$", C, NE);
+        dot("$A$", A, NW);
+        dot("$P$", P, NW);
+        dot("$Q$", Q, NW);
+        dot("$R$", R, NE);
+        dot("$T$", U, NW);
+        dot("$S$", S, NE);
+        dot("$U$", T, NW);
+        clip((-1,-0.3)--(1,-0.3)--(1,1.2)--(-1,1.2)--cycle);
+        [/asy]
+
+        this is a new line
+        this is a new line
+        this is another new line
+
+        [b]
+
+        some more random lorem text"""
+        with pytest.raises(SyntaxError) as context:
+            _ = validate_tags_and_setup_asy(
+                text, pathlib.Path("./"), pathlib.Path("./build/")
+            )
+        assert context.type is SyntaxError
+        assert "Failed to find an opening pair for delimiter." in str(
+            context.value
+        )
+        assert "39:" in str(context.value)
+        assert "[b]" in str(context.value)
