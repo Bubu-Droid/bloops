4cb78f86392ed32fddb0d1ec3a0cf2f36b3d561e
Bubu-Droid <fufudadw@gmail.com>
2026-09-15 23:45:20 +0530

    tests: add favicon and correct title to transpile test

--- tests/test_transpile.py
+++ tests/test_transpile.py
@@ -164,7 +164,12 @@ and [u]underline[/u] to top it off. [sup]14[/sup]P[sub]3[/sub]."""
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>bloops preview</title>
+    <title>Bloops Preview</title>
+    <link
+      rel="icon"
+      type="image/x-icon"
+      href="https://avatar.artofproblemsolving.com/avatar_778606.png"
+    />
     <link rel="stylesheet" href="static/style.css" />
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
