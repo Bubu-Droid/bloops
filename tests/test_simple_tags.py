a0a78ed67233009e83f4f75611d20d14e09f769f
Bubu-Droid <bubu@bubudroid.me>
2026-09-24 03:03:42 +0530

    tests: make intentional error to check pre-push hooks

--- tests/test_simple_tags.py
+++ tests/test_simple_tags.py
@@ -9,7 +9,8 @@ class TestSimpleTag:
         res = convert_to_html(
             text, 6, re.compile(r"\[(b)\]"), re.compile(r"\[/b\]")
         )
-        assert res == (r"""<strong>some random stuff</strong>""")
+        # assert res == (r"""<strong>some random stuff</strong>""")
+        assert res == (r"""<strong>ome random stuff</strong>""")
 
     def test_i_tag(self) -> None:
         text = r"""lorem [i]some random stuff[/i] lorem"""
