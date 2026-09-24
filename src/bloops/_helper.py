5e60aaec09de913e1783f8a72c09266fd1c39870
Bubu-Droid <bubu@bubudroid.me>
2026-09-25 01:07:23 +0530

    refactor: move internal functions to private helper module

---
+++ src/bloops/_helper.py
@@ -0,0 +1,33 @@
+import re
+
+
+def get_error_line(text: str, index: int) -> tuple[int, str]:
+    line_num = text.count("\n", 0, index)
+
+    return (line_num + 1, text.splitlines()[line_num])
+
+
+def get_tag_and_args(
+    text: str,
+    open_delim_start_index: int,
+    open_delim: re.Pattern[str],
+) -> tuple[str, dict[str, str]]:
+    match = open_delim.match(text, open_delim_start_index)
+    if not match:
+        error_line = get_error_line(text, open_delim_start_index)
+        raise SyntaxError(
+            "No opening delimiter at the current position."
+            + "\n\n"
+            + f"{error_line[0]}: {error_line[1]}"
+        )
+
+    pattern = r'[\w]+=".*?"|[\w]+=[^\s]+'
+    if len(match.groups()) <= 1:
+        return (match.group(1), {})
+
+    args_list: list[str] = re.findall(pattern, match.group(2))
+    args_dict = {
+        arg.split("=")[0]: (arg.split("=")[1]).strip('"') for arg in args_list
+    }
+
+    return (match.group(1), args_dict)
